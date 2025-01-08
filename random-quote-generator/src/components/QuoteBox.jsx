bd1522c6fa54a80e0d50d5d30a531ce28ac13391
Liam Appleton <liamappleton97@outlook.com>
2025-01-08 08:11:42 +0000

    adding fetchData function

--- random-quote-generator/src/components/QuoteBox.jsx
+++ random-quote-generator/src/components/QuoteBox.jsx
@@ -2,13 +2,44 @@ import React from "react";
 import styles from "./QuoteBox.module.css";
 
 const QuoteBox = () => {
+  const apiKey = "bWbwxDdtS9Bzsouymirguw==uZhWEMRD79j4eKVg";
+  const apiUrl = "https://api.api-ninjas.com/v1/quotes";
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          "X-Api-Key": apiKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <form action="" className={styles.container}>
+      <form
+        className={styles.container}
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchData();
+        }}
+      >
         <div className="mb-3">
-          <input type="text" className="form-control" readOnly="true" />
+          <input type="text" className="form-control" readOnly={true} />
         </div>
-        <button className="btn btn-primary">Generate quote</button>
+        <button type="submit" className="btn btn-primary">
+          Generate quote
+        </button>
       </form>
     </div>
   );
