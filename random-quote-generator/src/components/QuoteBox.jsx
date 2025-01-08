56a8135ae99c4ebea572fc6cbdbc498370369707
Liam Appleton <liamappleton97@outlook.com>
2025-01-08 07:57:11 +0000

    adding styling to QuoteBox components

--- random-quote-generator/src/components/QuoteBox.jsx
+++ random-quote-generator/src/components/QuoteBox.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import styles from "./QuoteBox.module.css";
 
 const QuoteBox = () => {
   return (
-    <form action="">
-      <div className="mb-3">
-        <input type="text" className="form-control" readOnly="true" />
-      </div>
-      <button className="btn btn-primary">Generate quote</button>
-    </form>
+    <div>
+      <form action="" className={styles.container}>
+        <div className="mb-3">
+          <input type="text" className="form-control" readOnly="true" />
+        </div>
+        <button className="btn btn-primary">Generate quote</button>
+      </form>
+    </div>
   );
 };
 
